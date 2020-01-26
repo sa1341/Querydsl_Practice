50b5853976eeea4bb8004f8cc9dc759bd34cd0a4
임준영 <a79007714@gmail.com>
2020-01-26 21:15:43 +0900

    Querydsl에서 제공하는 페이징 처리를 하는 메소드인 limit(), offset() 메소드를 이용하여 예제코드를 작성하였습니다. querydsl 집계함수인 sum(),avg(), max(),min() 를 이용하여 테이블에서 조회를 하였고, 조회한 컬럼의 타입이 다르기 때문에 List<Tuple>로 결과값을 반환받고, Tuple 객체를 통해서 원하는 값을 추출하 였습니다.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,22 +1,22 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Transactional
 @SpringBootTest
@@ -174,6 +174,102 @@ public class QuerydslBasicTest {
         assertThat(memberNull.getUsername()).isNull();
     }
 
+    @Test
+    public void paging1() throws Exception {
+                
+        //given
+        //when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
 
+        //then
+        assertThat(result.size()).isEqualTo(2);
+
+     }
+
+     @Test
+     public void paging2() throws Exception {
+
+         //given
+         //when
+         QueryResults<Member> queryResults = queryFactory
+                 .selectFrom(member)
+                 .orderBy(member.username.desc())
+                 .offset(1)
+                 .limit(2)
+                 .fetchResults();
+
+
+         //then
+         assertThat(queryResults.getTotal()).isEqualTo(4);
+         assertThat(queryResults.getLimit()).isEqualTo(2);
+         assertThat(queryResults.getOffset()).isEqualTo(1);
+         assertThat(queryResults.getResults().size()).isEqualTo(2);
+
+      }
+
+
+      @Test
+      public void aggregation() throws Exception {
+
+          //given
+          //when
+          List<Tuple> result = queryFactory
+                  .select(member.count(),
+                          member.age.sum(),
+                          member.age.avg(),
+                          member.age.max(),
+                          member.age.min())
+                  .from(member)
+                  .fetch();
+
+          //then
+
+
+          Tuple tuple = result.get(0);
+
+          assertThat(tuple.get(member.count())).isEqualTo(4);
+          assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+          assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+          assertThat(tuple.get(member.age.max())).isEqualTo(40);
+          assertThat(tuple.get(member.age.min())).isEqualTo(10);
+          
+      }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라.
+     *
+     */
+      @Test
+      public void group() throws Exception {
+                  
+          //given
+          //when
+          List<Tuple> result = queryFactory
+                  .select(team.name, member.age.avg())
+                  .from(member)
+                  .join(member.team, team)
+                  .groupBy(team.name)
+                  .fetch();
+
+          //then
+          Tuple teamA = result.get(0);
+          Tuple teamB = result.get(1);
+
+
+          assertThat(teamA.get(team.name)).isEqualTo("teamA");
+          assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+          assertThat(teamB.get(team.name)).isEqualTo("teamB");
+          assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+
+
+       }
+
+    
 
 }
