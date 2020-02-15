1f8bd8c0d37229eabc73df2326cf3f34b6aa66ef
임준영 <a79007714@gmail.com>
2020-02-16 01:59:02 +0900

    Spring data와 Querydsl을 이용한 페이징 처리 예제 코드 작성 및 카운트 쿼리 성능 최적화에 대한 고민을 담은 예제 작성

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,10 +1,17 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -61,4 +68,75 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
         return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> result = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<MemberTeamDto> content = result.getResults();
+        long total = result.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        // count 쿼리를 최적화 하기 위해서 별도로 콘텐츠만 조회하는 쿼리랑 count를 조회하는 쿼리를 분리하면 성능이 더 항상됩니다.
+        // 데이터가 많이 없으면 그냥 QueryResult 객체로 받아서 카운트 쿼리를 써도 되지만, 대용량 트래픽을 처리할 경우는 카운트 쿼리에 대해서 고민을 해봐야 합니다.
+        List<MemberTeamDto> content = getMemberTeamDtos(condition, pageable);
+        JPAQuery<Member> countQuery = getTotal(condition);
+
+        // content Size가 페이지 사이즈보다 작을 때는 카운트 쿼리를 날리지 않는 메소드입니다.
+        return PageableExecutionUtils.getPage(content, pageable, countQuery :: fetchCount);
+    }
+
+    private List<MemberTeamDto> getMemberTeamDtos(MemberSearchCondition condition, Pageable pageable) {
+        return queryFactory
+                    .select(new QMemberTeamDto(
+                            member.id.as("memberId"),
+                            member.username,
+                            member.age,
+                            team.id.as("teamId"),
+                            team.name.as("teamName")))
+                    .from(member)
+                    .leftJoin(member.team, team)
+                    .where(
+                            usernameEq(condition.getUsername()),
+                            teamNameEq(condition.getTeamName()),
+                            ageGoe(condition.getAgeGoe()),
+                            ageLoe(condition.getAgeLoe()))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetch();
+    }
+
+    private JPAQuery<Member> getTotal(MemberSearchCondition condition) {
+        return queryFactory
+                    .select(member)
+                    .from(member)
+                    .leftJoin(member.team, team)
+                    .where(
+                            usernameEq(condition.getUsername()),
+                            teamNameEq(condition.getTeamName()),
+                            ageGoe(condition.getAgeGoe()),
+                            ageLoe(condition.getAgeLoe())
+                    );
+
+    }
 }
