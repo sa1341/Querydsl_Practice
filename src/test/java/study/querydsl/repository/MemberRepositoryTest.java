769b25101e5eef772217a008315bc2139112029c
임준영 <a79007714@gmail.com>
2020-02-21 00:29:00 +0900

    Querydsl에서 제공하는 QuerydslPredicateExcuter<T>를 상속받아서 쿼리 메소드 예제를 작성하였습니다. 이 과정에서 QuerydslPredicateExcuter<T> 장단점을 알 수 있었습니다. 단일 테이블에서 JPQL을 작성할 때는 빠르고 간단하게 작성할 수 있지만 실무 환경과 같은 복잡한 환경에서 조인을 사용할 경우 left join을 사용할 수 없습니다. 또한 서비스나 컨트롤러 계층에서 querydsl 구현 기술에 의존성이 높기 때문에 클라이언트 코드에 영향을 줄 수 있다는 단점도 존재합니다. 복잡한 실무 환경에서 사용하기에는 한계가 존재하는 객체이기 때문에 이 점을 유의해야합니다.

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -121,5 +122,41 @@ class MemberRepositoryTest {
 
     }
 
+    /**
+     * 실무에서는 권장되지 않는 방법입니다.
+     * 1. 서비스 계층이나 컨트롤러 계층에서 queydsl이라는 구현 기술에 대해 의존적이기 때문에 기술 교체 시 클라이언트 코드에 영향이 갈 수 있습니다.
+     * 2. 묵시적 조인은 사용 가능하지만, letf join이 불가능 합니다.
+     * 3. 복잡한 실무환경에서 사용하기에는 한계가 명확합니다.
+     */
+
+   @Test
+    public void querydslPredicateExecutorTest() throws Exception {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);        //given
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+
+
+        //when
+        for (Member findMember : result){
+            System.out.println("member = " + findMember);
+        }
+
+
+        //then
+     }
 
 }
